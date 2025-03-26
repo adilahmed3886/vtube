b5e15cc8dcf3ba6303e65cd2d0bef6a6befae00d
adilahmed3886 <adilahmed3886@gmail.com>
2025-03-26 15:48:20 +0500

    add an api error managment utility

---
+++ src/utils/apiError.ts
@@ -0,0 +1,22 @@
+import { text } from "express";
+
+class ApiError extends Error {
+    constructor(
+        public statusCode: number,
+        public message: string = "Something went wrong",
+        public errors: Error[] = [],
+        public stack?: string,
+        public data: null = null,
+        public isSuccess: boolean = false,
+
+    ) {
+        super(message);
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+export { ApiError };
